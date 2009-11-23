d05585288d7ae4457f2613319b096c4dbbee52b0
Brian Slesinsky <brian-git@slesinsky.org>
2009-11-22 17:07:21 -0800

    finished required GTP commands, cleanup

--- gongo.go
+++ gongo.go
@@ -11,14 +11,62 @@ import (
 	"strings";
 )
 
-// === public definitions ===
+// The gongo package handles I/O for Go-playing robots written in Go.
 
-type Game interface {
+// Go-playing robots are normally implemented as command-line tools that
+// take commands from a controller on stdin and write responses to
+// stdout. The Go Text Protocol (GTP) [1] defines how this should be done. 
+// A robot that implements GTP can be plugged into various useful tools.
+// For example, GoGui [2] implements a user interface that will work with
+// any robot that implements GTP.
+// [1] http://www.lysator.liu.se/~gunnar/gtp/gtp2-spec-draft2/gtp2-spec.html
+// [2] http://gogui.sourceforge.net/
+
+// Executes GTP commands until "quit" is received, using the specified robot.
+// Returns nil for quit or non nil for I/O error. 
+func Run(robot GoRobot, input io.Reader, out io.Writer) os.Error {
+	in := bufio.NewReader(input);
+	for {
+		command, args, err := parseCommand(in);
+		if err != nil { return err; }
+
+		next_handler, ok := handlers[command];
+		if !ok {
+			fmt.Fprint(out, error("unknown command"));
+			continue;
+		}
+
+		fmt.Fprint(out, next_handler(request{robot, args}));
+
+		if command == "quit" { break; }
+	}
+	return nil;
+}
+
+type GoRobot interface {
+	// Attempts to change the board size. If the robot doesn't support the
+	// new size, return false. (In any case, board sizes above 25 aren't
+	// supported by GTP.)
+	// The controller should call ClearBoard next, or the results are undefined. 
 	SetBoardSize(size int) (ok bool);
+
 	ClearBoard();
 	SetKomi(komi float);
+
+	// Adds a move to the board. Moves can be added in any order, for example
+	// to set up a position or replay a game. The robot should automatically
+	// handle captures. If a move is illegal, return false.
 	Play(move Move) (ok bool);
+
+	// Asks the robot to generate a move at the current position for the given
+	// color. The robot may be asked to play either side, regardless of which
+	// side it was playing before.
+	// The robot should return a vertex (including pass) and handle captures
+	// automatically. Or it can resign by returning ok=false.
+	GenMove(color Color) (vertex Vertex, ok bool);
 }
+
+// Types used by the GoRobot interface
 
 type Color bool;
 const (
@@ -35,37 +83,39 @@ func ParseColor(input string) (c Color, ok bool) {
 }
 
 func (c Color) String() string {
-	if c {
-		return "White";
+	switch {
+	case c == White: return "White";
+	case c == Black: return "Black";
 	}
-	return "Black";
+	panic("not reachable");
 }
 
+// Identifies a place on the board to play a stone. The zero value is "pass".
+// The x index goes from left to right with a range of 1 to the board's size.
+// This is printed as letters starting from 'A', skipping 'I'.
+// The y index goes from bottom to top, from 1.
 type Vertex struct {
-	X int; // left to right, from 1 (printed as letters starting from 'A', skipping 'I'.)
-	Y int; // bottom to top, from 1
+	X int; 
+	Y int; 
 }
 
-const (
-	x_letters = "_ABCDEFGHJKLMNOPQRSTUVWXYZ"; // no I
-)
+const MaxBoardSize = 25;
 
 func ParseVertex(input string) (v Vertex, ok bool) {
 	input = strings.ToUpper(input);
-	if input == "PASS" {
-		return Vertex{}, true;
-	}
-	if len(input) < 2 { 
-		return Vertex{}, false;
-	}
-	x := strings.Index(x_letters, input[0:1]); 
-	if x < 1 {
-		return Vertex{}, false;
-	}
+	if len(input) < 2 { return Vertex{}, false; }
+
+	if input == "PASS" { return Vertex{}, true; }
+
+	x := 1 + int(input[0]) - int('A');
+	if (input[0] > 'I') { x--; }
+	if x < 1 || x > MaxBoardSize { return Vertex{}, false; }
+
 	y, err := strconv.Atoi(input[1:len(input)]); 
-	if err != nil {
+	if err != nil || y < 1 || y > MaxBoardSize {
 		return Vertex{}, false;
 	}
+
 	return Vertex{X: x, Y: y}, true;
 }
 
@@ -84,20 +134,18 @@ func (this Vertex) Equals(that Vertex) bool {
 func (v Vertex) String() string {
 	if v.IsPass() {
 		return "pass";
-	} else if !v.IsValid(25) {
+	} else if !v.IsValid(MaxBoardSize) {
 		return fmt.Sprintf("invalid: (%v,%v)", v.X, v.Y);
 	}
-	return fmt.Sprintf("%c%v", x_letters[v.X], v.Y );
+	x_letter := byte(v.X) - 1 + 'A';
+	if x_letter >= 'I' { x_letter--; }
+	return fmt.Sprintf("%c%v", x_letter, v.Y );
 }
 
 type Move struct {
 	Color Color;
 	Vertex Vertex;
 }
-
-var (
-	move_regexp = regexp.MustCompile("(w|white|b|black) ([a-z][0-9]+)");
-)
 
 func ParseMove(input string) (m Move, ok bool) {
 	words := strings.Split(input, " ", 0);
@@ -117,36 +165,27 @@ func (m Move) String() string {
 	return fmt.Sprintf("%v %v", m.Color, m.Vertex);
 }
 
-// Reads GTP commands and writes their results.
-// Returns nil when a quit command is read, or  non nil on error. 
-// For more on the Go Text Protocol, see: 
-// http://www.lysator.liu.se/~gunnar/gtp/
-func Run(game Game, input io.Reader, out io.Writer) os.Error {
-	in := bufio.NewReader(input);
+// === driver internals ===
+
+var word_regexp = regexp.MustCompile("[^  ]+")
+
+func parseCommand(in *bufio.Reader) (cmd string, args []string, err os.Error) {
 	for {
-		command, args, err := parseCommand(in);
-		if err != nil {
-			return err;
-		} else if command == "quit" {
-			fmt.Fprint(out, success(""));
-			break;						
-		}
-		next_handler, ok := handlers[command];
-		if ok {
-			fmt.Fprint(out, next_handler(request{game, args}))
-		} else {
-			fmt.Fprint(out, error("unknown command"));			
+		line, err := in.ReadString('\n');
+		if err != nil { return "", nil, err; }
+		line = strings.TrimSpace(line);
+		if line != "" && line[0] != '#' {
+			words := word_regexp.AllMatchesString(line, 0);
+			return words[0], words[1:len(words)], nil;
 		}
 	}
-	return nil;
+	return "", nil, os.NewError("shouldn't get here");
 }
-
-// === GTP protocol ===
 
 type handler func (request) response;
 
 type request struct {
-	game Game;
+	robot GoRobot;
 	args []string;
 }
 
@@ -164,10 +203,9 @@ func error(message string) response {
 }
 
 func (r response) String() string {
-	if !r.success {
-		return "? " + r.message + "\n\n"
-	}
-	return "= " + r.message + "\n\n"
+	prefix := "=";
+	if !r.success { prefix = "?" }
+	return prefix + " " + r.message + "\n\n";
 }
 
 var (
@@ -176,98 +214,85 @@ var (
 	_list = func(req request) response { return handle_list_commands(req) };
 
 	handlers = map[string] handler {
-		"protocol_version" : func(req request) response { return success("2") },
-		"name" : func(req request) response { return success("gongo") },
-		"version" : func(req request) response { return success("") },
-		"known_command" : _known,
-		"list_commands": _list,
 		"boardsize": handle_boardsize,
-		"clear_board": func (req request) response { req.game.ClearBoard(); return success(""); },
+		"clear_board": func (req request) response { req.robot.ClearBoard(); return success(""); },
+		"genmove": handle_genmove,
+		"known_command" : _known,
 		"komi": handle_komi,
+		"list_commands": _list,
+		"name" : func(req request) response { return success("gongo") },
 		"play": handle_play,
-		"quit" : nil
+		"protocol_version" : func(req request) response { return success("2") },
+		"quit" : func (req request) response { return success("") },
+		"version" : func(req request) response { return success("") },
+
 	};
-	word_regexp = regexp.MustCompile("[^  ]+")
 )
 
 func handle_known_command(req request) response {
-	if len(req.args) != 1 {
-		return error("wrong number of arguments");
-	}
+	if len(req.args) != 1 { return error("wrong number of arguments"); }
+
 	_, ok := handlers[req.args[0]];
-	if ok {
-		return success("true");
-	}
-	return success("false");
+	return success(fmt.Sprint(ok));
 }
 
 func handle_list_commands(req request) response {
-	if len(req.args) != 0 {
-		return error("wrong number of arguments");
-	}
+	if len(req.args) != 0 { return error("wrong number of arguments"); }
+
 	names := make([]string, len(handlers));
 	i := 0;
 	for name := range handlers {
 		names[i] = name;
 		i++;
 	}
+
 	sort.SortStrings(names);
 	return success(strings.Join(names, "\n"));
 }
 
 func handle_boardsize(req request) response {
-	if len(req.args) != 1 {
-		return error("wrong number of arguments");
-	}
+	if len(req.args) != 1 { return error("wrong number of arguments"); }
+
 	size, err := strconv.Atoi(req.args[0]);
-	if err != nil {
+	if err != nil { return error("unacceptable size"); }
+
+	if !req.robot.SetBoardSize(size) {
 		return error("unacceptable size");
 	}
-	if !req.game.SetBoardSize(size) {
-		return error("unacceptable size");
-	}
+
 	return success("");
 }
 
 func handle_komi(req request) response {
-	if len(req.args) != 1 {
-		return error("wrong number of arguments");
-	}
+	if len(req.args) != 1 { return error("wrong number of arguments"); }
+	
 	komi, err := strconv.Atof(req.args[0]);
-	if err != nil {
-		return error("syntax error");
-	}
-	req.game.SetKomi(komi);
+	if err != nil { return error("syntax error"); }
+	
+	req.robot.SetKomi(komi);
 	return success("");
 }
 
 func handle_play(req request) response {
-	if len(req.args) != 2 {
-		return error("wrong number of arguments");
-	}
+	if len(req.args) != 2 { return error("wrong number of arguments"); }
+
 	move, ok := ParseMove(req.args[0] + " " + req.args[1]);
-	if !ok {
-		return error("syntax error");
-	}
-	if !req.game.Play(move) {
-		return error("illegal move");
-	}
+	if !ok { return error("syntax error"); }
+
+	ok = req.robot.Play(move);
+	if !ok { return error("illegal move"); }
+
 	return success("");
 }
 
-func parseCommand(in *bufio.Reader) (string, []string, os.Error) {
-	for {
-		line, err := in.ReadString('\n');
-		if err != nil {
-			return "", nil, err;
-		}
-		line = strings.TrimSpace(line);
-		if line != "" && line[0] != '#' {
-			words := word_regexp.AllMatchesString(line, 0);
-			return words[0], words[1:len(words)], nil;
-		}
-	}
-	return "", nil, os.NewError("shouldn't get here");
-}
+func handle_genmove(req request) response {
+	if len(req.args) != 1 { return error("wrong number of arguments"); }
 
-// === The game ===
+	color, ok := ParseColor(req.args[0]);
+	if !ok { return error("syntax error"); }		
+
+	vertex, ok := req.robot.GenMove(color);
+	if !ok { return success("resign"); }
+
+	return success(vertex.String());
+}
--- gongo_test.go
+++ gongo_test.go
@@ -24,6 +24,7 @@ func TestListCommands(t *testing.T) {
 	checkCommand(t, nil, "list_commands",
 `boardsize
 clear_board
+genmove
 known_command
 komi
 list_commands
@@ -35,7 +36,7 @@ version`);
 }
 
 func TestBoardSize(t *testing.T) {
-	g := &fake_game{};
+	g := NewFakeRobot();
 	checkCommand(t, g, "boardsize 9", "");
 	if g.board_size != 9 {
 		t.Errorf("expected board size %v but got %v", 9, g.board_size);
@@ -43,7 +44,7 @@ func TestBoardSize(t *testing.T) {
 }
 
 func TestClearBoard(t *testing.T) {
-	g := &fake_game{};
+	g := NewFakeRobot();
 	checkCommand(t, g, "clear_board", "");
 	if !g.board_cleared {
 		t.Errorf("board not cleared");
@@ -51,7 +52,7 @@ func TestClearBoard(t *testing.T) {
 }
 
 func TestKomi(t *testing.T) {
-	g := &fake_game{};
+	g := NewFakeRobot();
 	checkCommand(t, g, "komi 6.5", "");
 	if g.komi != 6.5 {
 		t.Errorf("expected komi %v but got %v", 6.5, g.komi);
@@ -59,7 +60,7 @@ func TestKomi(t *testing.T) {
 }
 
 func TestPlay(t *testing.T) {
-	g := &fake_game{};
+	g := NewFakeRobot();
 	checkCommand(t, g, "play white c10", "");
 	expected := Move{White, Vertex{3,10}};
 	if !expected.Equals(g.move) {
@@ -67,13 +68,22 @@ func TestPlay(t *testing.T) {
 	}
 }
 
-func TestParseMove(t *testing.T) {
-	checkMove(t, "b pass", Black, 0, 0);
-	checkMove(t, "w Pass", White, 0, 0);
-	checkMove(t, "B a1", Black, 1, 1);	
-	checkMove(t, "black H8", Black, 8, 8);
-	checkMove(t, "b j9", Black, 9, 9);
-	checkMove(t, "WHITE T19", White, 19, 19);
+func TestGenmove(t *testing.T) {
+	g := NewFakeRobot();
+	g.send_vertex = Vertex{3,10};
+	checkCommand(t, g, "genmove black", "C10");
+	if Black != g.color {
+		t.Errorf("expected %v but got %v", Black, g.color);
+	}
+}
+
+func TestGenmove_Resign(t *testing.T) {
+	g := NewFakeRobot();
+	g.send_ok = false;
+	checkCommand(t, g, "genmove white", "resign");
+	if White != g.color {
+		t.Errorf("expected %v but got %v", White, g.color);
+	}
 }
 
 func TestUnknownCommandError(t *testing.T) {
@@ -85,6 +95,15 @@ func TestQuit(t *testing.T) {
 	checkRun(t, nil, "# comment\n\nquit\n",  "= \n\n");
 }
 
+func TestParseMove(t *testing.T) {
+	checkMove(t, "b pass", Black, 0, 0);
+	checkMove(t, "w Pass", White, 0, 0);
+	checkMove(t, "B a1", Black, 1, 1);	
+	checkMove(t, "black H8", Black, 8, 8);
+	checkMove(t, "b j9", Black, 9, 9);
+	checkMove(t, "WHITE T19", White, 19, 19);
+}
+
 // === end of tests ===
 
 type fake_game struct {
@@ -92,11 +111,18 @@ type fake_game struct {
 	board_cleared bool;
 	komi float;
 	move Move;
+	color Color;
+	send_vertex Vertex;
+	send_ok bool;
+}
+
+func NewFakeRobot() *fake_game {
+	return &fake_game{send_ok: true};
 }
 
 func (g *fake_game) SetBoardSize(value int) bool {
 	g.board_size = value;
-	return true;
+	return g.send_ok;
 }
 
 func (g *fake_game) ClearBoard() {
@@ -109,7 +135,12 @@ func (g *fake_game) SetKomi(value float) {
 
 func (g *fake_game) Play(value Move) bool {
 	g.move = value;
-	return true;
+	return g.send_ok;
+}
+
+func (g *fake_game) GenMove(color Color) (vertex Vertex, ok bool) {
+	g.color = color;
+	return g.send_vertex, g.send_ok;
 }
 
 func checkMove(t *testing.T, input string, expectedColor Color, expectedX int, expectedY int) {
@@ -119,21 +150,21 @@ func checkMove(t *testing.T, input string, expectedColor Color, expectedX int, e
 		return;
 	}
 	if expectedColor != actual.Color {
-		t.Error("unexpected color for %v", input);
+		t.Error("unexpected color for", input);
 	}
 	if expectedX != actual.Vertex.X {
-		t.Error("unexpected X for %v", input);	
+		t.Error("unexpected X for", input, "Got:", actual.Vertex.X);	
 	}
 	if expectedY != actual.Vertex.Y {
-		t.Error("unexpected Y for %v", input);	
+		t.Error("unexpected Y for", input, "Got:", actual.Vertex.Y);	
 	}
 }
 
-func checkCommand(t *testing.T, g Game, input, expected string) {
+func checkCommand(t *testing.T, g GoRobot, input, expected string) {
 	checkRun(t, g, input + "\nquit\n", "= " + expected + "\n\n= \n\n");
 }
 
-func checkRun(t *testing.T, g Game, input, expected string) {
+func checkRun(t *testing.T, g GoRobot, input, expected string) {
 	actual := new(bytes.Buffer);
 	var result = Run(g, bytes.NewBufferString(input), actual);
 	if expected != actual.String() {
