7bf998b0e5aff3804e5726ba41c6effd03e0bb23
Brian Slesinsky <brian-git@slesinsky.org>
2009-11-26 15:45:15 -0800

    start on robot tests

--- gongo_robot_test.go
+++ gongo_robot_test.go
@@ -1,10 +1,52 @@
 package gongo
 
 import (
+	"bytes";
+	"strings";
 	"testing";
 )
 
-func TestBasics(t *testing.T) {
+func TestNewRobot(t *testing.T) {
 	r := NewRobot(3);
-	if r.GetBoardSize() != 3 { t.Error("board is wrong size"); }
+	CheckBoard(t, r,
+`...
+ ...
+ ...`);
+}
+
+
+// == end of tests ===
+
+func CheckBoard(t *testing.T, r GoRobot, expectedBoard string) {
+	expected := TrimBoard(expectedBoard);
+	actual := LoadBoard(r);
+	if expected != actual {
+		t.Errorf("board is different. Expected:\n%v\nActual:\n%v\n", expected, actual);
+	}
+}
+
+func TrimBoard(s string) string {
+	lines := strings.Split(s, "\n", 0);
+	for i := range lines {
+		lines[i] = strings.TrimSpace(lines[i]);
+	}
+	return strings.Join(lines, "\n");
+}
+
+func LoadBoard(r GoRobot) string {
+	var out bytes.Buffer;
+	size := r.GetBoardSize();
+	for y := size; y >= 1; y-- {
+		for x := 1; x <= size; x++ {
+			switch r.GetCell(x,y) {
+			case Empty: out.WriteString(".");
+			case White: out.WriteString("O");
+			case Black: out.WriteString("@");
+			}
+		}
+		if y > 1 {
+			out.WriteString("\n");
+		}
+	}
+	return out.String();
 }
