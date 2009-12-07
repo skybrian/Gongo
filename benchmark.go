d943ee9637f43055d030f40a8731fca1c60546b6
Brian Slesinsky <brian-git@slesinsky.org>
2009-12-06 16:22:00 -0800

    add another argument to benchmark to run games repeatedly

--- benchmark.go
+++ benchmark.go
@@ -14,23 +14,34 @@ func UsageError() {
 
 func main() {
 	moveCount := 10;
-	if len(os.Args) == 2 {
+	gameCount := 1;
+	if len(os.Args) >= 2 {
 		val, err := strconv.Atoi(os.Args[1]);
 		if err != nil {
 			UsageError()
 		}
 		moveCount = val;
-	} else if len(os.Args) > 2 {
+	}
+	if len(os.Args) >= 3 {
+		val, err := strconv.Atoi(os.Args[2]);
+		if err != nil {
+			UsageError()
+		}
+		gameCount = val;
+	}
+	if len (os.Args) > 3 {
 		UsageError()
 	}
 
 	var conf gongo.Config;
 	conf.BoardSize = 9;
-	r := gongo.NewConfiguredRobot(conf);
-	color := gongo.Black;
-	for i := 0; i < moveCount; i++ {
-		r.GenMove(color);
-		color = color.GetOpponent();
+	for game := 0; game < gameCount; game++ {
+		r := gongo.NewConfiguredRobot(conf);
+		color := gongo.Black;
+		for i := 0; i < moveCount; i++ {
+			r.GenMove(color);
+			color = color.GetOpponent();
+		}
+		fmt.Println(gongo.BoardToString(r));
 	}
-	fmt.Println(gongo.BoardToString(r));
 }
