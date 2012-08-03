dc69958059c1ab5eca202c574bd0a7eb18796ed5
Brian Slesinsky <brian-git@slesinsky.org>
2012-08-03 09:12:43 -0700

    oops, forgot to add this

---
+++ gongo-benchmark/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/skybrian/Gongo"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func UsageError() {
+	fmt.Fprintf(os.Stderr, "Usage: %v [moveCount]\n\n", os.Args[0])
+	os.Exit(1)
+}
+
+func main() {
+	moveCount := 10
+	gameCount := 1
+	if len(os.Args) >= 2 {
+		val, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			UsageError()
+		}
+		moveCount = val
+	}
+	if len(os.Args) >= 3 {
+		val, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			UsageError()
+		}
+		gameCount = val
+	}
+	if len(os.Args) > 3 {
+		UsageError()
+	}
+
+	var conf gongo.Config
+	conf.BoardSize = 9
+	for game := 0; game < gameCount; game++ {
+		r := gongo.NewConfiguredRobot(conf)
+		color := gongo.Black
+		for i := 0; i < moveCount; i++ {
+			r.GenMove(color)
+			color = color.GetOpponent()
+		}
+		fmt.Println(gongo.BoardToString(r))
+	}
+}
