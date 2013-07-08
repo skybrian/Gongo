1c701d27f547cd8d9de2ca7c55f25a955358abce
antonio <antonio@happylatte.com>
2013-07-08 14:54:42 +0800

    Initial benchmarking

---
+++ robot_bench_test.go
@@ -0,0 +1,29 @@
+package gongo
+
+import (
+	"testing"
+	"math/rand"
+)
+
+func Benchmark9x9RandomGame(bench *testing.B) {
+	b := new(board)
+	b.clearBoard(9)
+	eboard := new(board)
+	eboard.clearBoard(9)
+	rng := rand.New(rand.NewSource(int64(2131)))
+	bench.ResetTimer()
+	for i := 0; i < bench.N; i++ {
+		b.playRandomGame(rng)
+		b.copyFrom(eboard)
+	}
+}
+
+func Benchmark9x9GenMove(b *testing.B) {
+	robot := NewRobot(9)
+	b.ResetTimer()
+	color := Black
+	for i := 0; i < b.N; i++ {
+		robot.GenMove(color)
+		color=color.GetOpponent()
+	}
+}
